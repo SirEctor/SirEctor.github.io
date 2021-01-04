07cb4262c6472db146a218344b2433ff2fe7dcdb
SirEctor <70787342+SirEctor@users.noreply.github.com>
2021-01-04 13:15:59 -0500

    random.js
    random quotes?

---
+++ scripts/random.js
@@ -0,0 +1,3 @@
+quotes = ["Talent is just pursued interest - Bob Ross", "To be or not to be - Shakespeare ","I could get a job that probably pays the bills... - NF"];
+console.log(quotes[Math.floor(Math.random() * quotes.length)]);
+

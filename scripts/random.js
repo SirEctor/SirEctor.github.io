61e16f8b9b9c8e712e92767f2dda5c00fb98bea9
SirEctor <70787342+SirEctor@users.noreply.github.com>
2021-01-04 13:31:51 -0500

    on load

--- scripts/random.js
+++ scripts/random.js
@@ -1,3 +1,5 @@
-quotes = ["Talent is just pursued interest - Bob Ross", "To be or not to be - Shakespeare ","I could get a job that probably pays the bills... - NF"];
-document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+window.onload = function() {
+  quotes = ["Talent is just pursued interest - Bob Ross", "To be or not to be - Shakespeare ","I could get a job that probably pays the bills... - NF"];
+  document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+}
 

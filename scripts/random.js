952ace1d2557a2ee5dde030082b2ce0d8f53b579
SirEctor <ebenezer.teshome@duke.edu>
2024-07-24 19:24:00 -0400

    version 2 START

--- scripts/random.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-  quotes = ["Talent is just pursued interest - Bob Ross", "To be or not to be - Shakespeare ","I could get a job that probably pays the bills... - NF"];
-  document.getElementById("quote").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
-}
-

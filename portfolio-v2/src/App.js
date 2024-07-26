7082a74e7912b7ba004fbf4a3f25d683fadc4c20
SirEctor <ebenezer.teshome@duke.edu>
2024-07-26 15:30:11 -0400

    using material ui once more

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { Button } from "@mui/material";
+
 
 function restOfAppPage(){
   return(
-    <div className="App-header">
-      <h1>About</h1>
-      <h1>Skills</h1>
-      <h1>Projects</h1>
-      <h1>Contact</h1>
+    <div className="App-header" sx={{ flexDirection: "column",  m: 1, minWidth: 120, flex: 1}}>
+      <Button variant="outlined"  sx={{mt: 2, color: "darkGray", fontSize:"4vw"}}  size="large">About</Button>
+      <Button variant="outlined"  sx={{mt: 2, color: "darkGray", fontSize:"4vw"}}  size="large">Skills</Button>
+      <Button variant="outlined"  sx={{mt: 2, color: "darkGray", fontSize:"4vw"}}  size="large">Projects</Button>
+      <Button variant="outlined"  sx={{mt: 2, color: "darkGray", fontSize:"4vw"}}  size="large">Contact</Button>
+      <h6 sx={{mt: 2}} style={{color: "#a9a9a9"}}>Created by E. Teshome (2024)</h6>
     </div>
   )
 }
